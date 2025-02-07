3fead731a88f33c9b8cd0d5d1762f7f366280a35
yriyriyri <Oscar.paterson93@gmail.com>
2025-02-08 08:36:43 +1300

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
